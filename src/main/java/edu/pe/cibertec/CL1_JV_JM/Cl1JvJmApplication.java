65dde3c13d88ec5b98911efc2ab6837705c7b867
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-09-23 19:00:20 -0500

    Add files via upload

---
+++ src/main/java/edu/pe/cibertec/CL1_JV_JM/Cl1JvJmApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.CL1_JV_JM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JvJmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JvJmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/CL1_JV_JM/Cl1JvJmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.CL1_JV_JM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JvJmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
