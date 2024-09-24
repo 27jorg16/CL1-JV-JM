3ae44d7db356575cff33ce514ccc3daa84302843
Juan <juancarlosmazuelos14@gmail.com>
2024-09-23 21:01:25 -0500

    application

--- src/main/java/edu/pe/cibertec/CL1_JV_JM/Cl1JvJmApplication.java
+++ src/main/java/edu/pe/cibertec/CL1_JV_JM/Cl1JvJmApplication.java
@@ -1,13 +1,36 @@
 package edu.pe.cibertec.CL1_JV_JM;
 
+import edu.pe.cibertec.CL1_JV_JM.model.Producto;
+import edu.pe.cibertec.CL1_JV_JM.service.FileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-public class Cl1JvJmApplication {
+public class Cl1JvJmApplication implements CommandLineRunner {
+	private final FileService fileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cl1JvJmApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Producto producto = new Producto("Laptop", 1200.00, "Electrónica");
+
+		// Crear y leer XML
+		fileService.crearArchivoXML(producto);
+		Producto productoXML = fileService.leerArchivoXML();
+		log.info("Producto XML: " + productoXML.toString());
+
+		// Crear y leer JSON
+		fileService.crearArchivoJSON(producto);
+		Producto productoJSON = fileService.leerArchivoJSON();
+		log.info("Producto JSON: " + productoJSON.toString());
+	}
 }
+
